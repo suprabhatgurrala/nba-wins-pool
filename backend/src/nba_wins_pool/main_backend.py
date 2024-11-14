30cdf46ad08422e13ad3d9cf6c53d964df0605ef
Ejnar Arechavala <ejnar.arechavala@onepeloton.com>
2024-11-13 22:08:16 -0800

    refactor + add vue

--- backend/src/nba_wins_pool/main_backend.py
+++ backend/src/nba_wins_pool/main_backend.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+import os
 
 from nba_wins_pool.nba_data import generate_leaderboard
 
 app = FastAPI()
 
+if os.getenv("SERVE_STATIC_FILES") == "true":
+    app.mount("/", StaticFiles(directory="static", html=True), name="static")   
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/leaderboard", response_class=HTMLResponse)
 def root():
     leaderboard_df = generate_leaderboard()
     return leaderboard_df.to_html()
--- makefile
+++
@@ -1,3 +0,0 @@
-docker-dev:
-	docker-compose -f docker-compose.yml up --build --watch
-
