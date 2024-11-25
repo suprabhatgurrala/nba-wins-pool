a2354712fd2d126c2d5bbe44d41cf7581eb69213
Ejnar Arechavala <ejnar123@gmail.com>
2024-11-24 20:14:04 -0800

    Server frontend from backend server, initial UI, some infra cleanup (#6)

--- backend/src/nba_wins_pool/main_backend.py
+++ backend/src/nba_wins_pool/main_backend.py
@@ -3,18 +3,18 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-
-from nba_wins_pool.nba_data import generate_leaderboard
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from .api import router as api_router
 
 app = FastAPI()
 
 
-@app.get("/leaderboard", response_class=HTMLResponse)
-def root():
-    leaderboard_df = generate_leaderboard()
-    return leaderboard_df.to_html()
-
+app.include_router(api_router, prefix="/api")
 
 if os.getenv("SERVE_STATIC_FILES") == "true":
     # This should be done after all routes
     app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if os.getenv("CORS_ALLOW_ALL") == "true":
+    app.add_middleware(CORSMiddleware, allow_origins=["*"])
