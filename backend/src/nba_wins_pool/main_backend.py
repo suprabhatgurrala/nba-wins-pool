73a88b1094e52c43a7262ad5720e0a898eb1ce94
Ejnar Arechavala <ejnar123@gmail.com>
2024-11-30 04:57:52 -0500

    fix routing (#27)

--- backend/src/nba_wins_pool/main_backend.py
+++ backend/src/nba_wins_pool/main_backend.py
@@ -3,9 +3,9 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 from .api import router as api_router
+from .utils.spa_static_files import SinglePageApplication
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,7 +22,7 @@ logger.info("CORS_ALLOW_ALL=%s", CORS_ALLOW_ALL)
 
 if SERVE_STATIC_FILES == "true":
     # This should be done after all routes
-    app.mount("/", StaticFiles(directory="static", html=True), name="static")
+    app.mount("/", SinglePageApplication(directory="static"), name="static")
 
 if CORS_ALLOW_ALL == "true":
     app.add_middleware(CORSMiddleware, allow_origins=["*"])
